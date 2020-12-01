fd4ca48c4c92816b5f7cd9b7c1102f01e562c7ee
uo269728 <uo269728@uniovi.es>
2020-12-01 17:15:50 +0100

    jjjj

--- practica3/Ejercicio12/Ejercicio12.js
+++ practica3/Ejercicio12/Ejercicio12.js
@@ -2,7 +2,7 @@
 // Obtener un archivo JSON con los datos de divisas
 
 "use strict";
-class  Cargador {
+class Cargador {
     constructor() {
 
     }
@@ -10,38 +10,38 @@ class  Cargador {
 
 
 
-    metodo(files){
+    metodo(files) {
 
-    var archivo = files[0];
-    $("#name").text("Nombre del archivo: " + archivo.name );
-    $("#size").text("Tamaño del archivo: " + archivo.size );
-    $("#type").text("Tipo del archivo: " + archivo.type );
-    $("#date").text("Fecha de la última modificación: " + archivo.lastModifiedDate );
-    $("#content").text("Contenido del archivo de texto:");
+        var archivo = files[0];
 
-    //Solamente admite archivos de tipo texto
-    var tipoTexto1 = 'text/plain';
-    var tipoTexto2 = 'text/json';
-    var tipoTexto3 = 'text/xml';
-    if (archivo.type.match(tipoTexto1) || archivo.type.match(tipoTexto2)||archivo.type.match(tipoTexto3)) 
-       {
-        var lector = new FileReader();
-        lector.onload = function (evento) {
-          //El evento "onload" se lleva a cabo cada vez que se completa con éxito una operación de lectura
-          //La propiedad "result" es donde se almacena el contenido del archivo
-          //Esta propiedad solamente es válida cuando se termina la operación de lectura
-          $("#contenido").text( lector.result)
-          }      
-        lector.readAsText(archivo);
+        var tipoTexto1 = 'text/plain';
+        var tipoTexto2 = 'application/json';
+        var tipoTexto3 = 'text/xml';
+        if (archivo.type.match(tipoTexto1) || archivo.type.match(tipoTexto2) || archivo.type.match(tipoTexto3)) {
+            var lector = new FileReader();
+            lector.onload = function (evento) {
+                $("#name").text("Nombre del archivo: " + archivo.name);
+                $("#size").text("Tamaño del archivo: " + archivo.size);
+                $("#type").text("Tipo del archivo: " + archivo.type);
+                $("#date").text("Fecha de la última modificación: " + archivo.lastModifiedDate);
+                $("#content").text("Contenido del archivo de texto:");
+                $("#contenido").text(lector.result)
+            }
+            lector.readAsText(archivo);
         }
-    else {
-        $("#contenido").text( "Error:  Archivo no válido");
-        }   
+        else {
+            $("#name").text("Nombre del archivo: " );
+            $("#size").text("Tamaño del archivo: " );
+            $("#type").text("Tipo del archivo: " );
+            $("#date").text("Fecha de la última modificación: ");
+            $("#contenido").text("Error:  Archivo no válido");
 
-        
+        }
+
+
     }
 
-    
+
 }
 var car = new Cargador();
 
